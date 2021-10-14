a262b8dea4082d9057b65bea68942160ecb5b925
Mike Fanger <mike.fanger@dynatrace.com>
2021-10-14 01:01:33 -0400

    Add support for filename parameter in client

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,9 +2,15 @@ import { ResponseEncoding } from 'axios';
 
 export type PdfEncoding = ResponseEncoding;
 
-export interface RenderPdfOptions {
+export interface CommonPdfOptions {
+  filename?: string;
+}
+
+export interface RenderPdfOptions extends CommonPdfOptions {
   encoding: PdfEncoding;
 }
+
+export type RenderPdfToStreamOptions = CommonPdfOptions;
 
 export interface Response<DataType> {
   data: DataType;
